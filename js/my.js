279c9f1cf00837cbce4430b9f7a09a7d85a14bfe
Vikash <vsahu24@gmail.com>
2017-05-22 20:31:42 -0700

    basic counter functionality works

--- js/my.js
+++ js/my.js
@@ -1,5 +1,13 @@
-$("a").click(function() {
-	var score = parseInt($("a").text());
-	score = score + 1;
-	$("a").text(score);
+$(document).ready(function() {
+	$("a#p1").click(function() {
+		var score = parseInt($("a#p1").text());
+		score = score + 1;
+		$("a#p1").text(score);
+	});
+	
+	$("a#p2").click(function() {
+		var score = parseInt($("a#p2").text());
+		score = score + 1;
+		$("a#p2").text(score);
+	});
 });

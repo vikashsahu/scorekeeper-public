95dea9574ce15bc9657a48074fa7d975f40078ad
Vikash <vsahu24@gmail.com>
2017-06-11 11:56:46 -0700

    try hiding view container until signin completes

--- js/my.js
+++ js/my.js
@@ -134,6 +134,8 @@ function setSigninStatus(isSignedIn) {
 		$('#revoke-access-button').css('display', 'inline-block');
 		$('#auth-status').html('You are currently signed in and have granted ' +
 			'access to this app.');
+		//display the scorekeeper view
+		$('.container').css('display', 'table');
 	} else {
 		$('#sign-in-or-out-button').html('Sign In/Authorize');
 		$('#revoke-access-button').css('display', 'none');
